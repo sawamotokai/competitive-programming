044a8d8c3e5642a4714fd2282f27718a79159821
Kai Sawamoto <kaisawamoto@gmail.com>
2020-03-03 06:56:13 -0800

    wrong

---
+++ CodeForce/rounds/ozonTechChallenge/a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef long long ll;
+const ll INF = 1e18L + 1;
+
+int main()
+{
+  int t;
+  cin >> t;
+  rep(_, t)
+  {
+    int n;
+    cin >> n;
+    vi a(n);
+    vi b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+    vi ans(n);
+    rep(i, n) rep(j, n) rep(k, n)
+    {
+      if ()
+    }
+  }
+  return 0;
+}
