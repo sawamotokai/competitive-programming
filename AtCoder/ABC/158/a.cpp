9bdb42c9385fa2e2c0e1565262b87bffa8ecf7ef
Kai Sawamoto <kaisawamoto@gmail.com>
2020-03-07 04:35:05 -0800

    a,b

---
+++ AtCoder/ABC/158/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef long long ll;
+const ll INF = 1e18L + 1;
+
+int main()
+{
+  string s;
+  cin >> s;
+  if (s[0] == s[2] && s[1] == s[0])
+    puts("No");
+  else
+    puts("Yes");
+  return 0;
+}
