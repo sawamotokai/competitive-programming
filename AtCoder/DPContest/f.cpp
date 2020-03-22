a03f674487e7505c475bf50cbf73254bb95193ca
Kai Sawamoto <kaisawamoto@gmail.com>
2020-03-21 21:21:18 -0700

    a

--- AtCoder/DPContest/f.cpp
+++ AtCoder/DPContest/f.cpp
@@ -11,26 +11,44 @@ typedef map<string, int> msi;
 typedef long long ll;
 const ll INF = 1e18L + 1;
 
+int dp[3010][3010];
+
 int main()
 {
   string s, t;
   cin >> s >> t;
-  vvi dp(s.length() + 1, vector<int>(t.length() + 1, 0));
-  // dp[i][j] = max total lenght so far
-  rep(i, s.length())
-  {
-    rep(j, t.length())
+  for (int i = 0; i < s.length(); i++)
+    for (int j = 0; j < t.length(); j++)
     {
       if (s[i] == t[j])
       {
-        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + 1);
+        dp[i + 1][j + 1] = dp[i][j] + 1;
       }
-      dp[i + 1][j] = max(dp[i][j], dp[i + 1][j]);
-      dp[i][j + 1] = max(dp[i][j], dp[i][j + 1]);
+      else
+      {
+        dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+      }
+    }
+  vector<char> ans;
+  int i = s.length(), j = t.length();
+  while (i > 0 && j > 0)
+  {
+    if (s[i - 1] == t[j - 1])
+    {
+      ans.push_back(s[i - 1]);
+      i--;
+      j--;
+    }
+    else
+    {
+      if (dp[i][j - 1] > dp[i][j])
+        j--;
+      else
+        i--;
     }
   }
-  int ans = 0;
-  rep(i, s.length() + 1) rep(j, t.length() + 1) ans = max(ans, dp[i][j]);
-  std::cout << ans << std::endl;
+  reverse(ans.begin(), ans.end());
+  rep(i, ans.size()) cout << ans[i];
+  cout << endl;
   return 0;
 }
---
+++ CodeForce/rounds/global/7/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef long long ll;
+const ll INF = 1e18L + 1;
+
+int main()
+{
+  int t;
+  cin >> t;
+  rep(_, t)
+  {
+    int n;
+    cin >> n;
+  }
+  return 0;
+}
