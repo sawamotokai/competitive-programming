ddee2b8b7665355f86ebf3183c76591c8ada0bb1
Kai Sawamoto <kaisawamoto@gmail.com>
2020-03-31 08:21:20 -0700

    cannt write union find

---
+++ AtCoder/ABC/160/e.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef greater<int> gt;
+typedef priority_queue<int, vector<int>, gt> minq;
+typedef long long ll;
+const ll INF = 1e18L + 1;
+
+bool comp(ii a, ii b)
+{
+  return a.first > b.first;
+}
+
+int main()
+{
+  int x, y, a, b, c;
+  cin >> x >> y >> a >> b >> c;
+  vii apples;
+  rep(i, a)
+  {
+    int x;
+    cin >> x;
+    apples.push_back(make_pair(x, 1));
+  }
+  rep(i, b)
+  {
+    int x;
+    cin >> x;
+    apples.push_back(make_pair(x, 2));
+  }
+  rep(i, a)
+  {
+    int x;
+    cin >> x;
+    apples.push_back(make_pair(x, 3));
+  }
+  sort(apples.begin(), apples.end(), comp);
+  int c1 = 0, c2 = 0;
+  rep(i, a + b + c)
+  {
+    ii apple = apples[i];
+    if (apple.second == 1)
+  }
+  return 0;
+}
