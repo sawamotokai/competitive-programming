504947a5d8d7b56f61ed035526709eaa04b8b392
Kai Sawamoto <kaisawamoto@gmail.com>
2020-04-22 19:37:11 +0900

    d

---
+++ AtCoder/problems/3/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ok() puts(ok?"Yes":"No");
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef greater<int> gt;
+typedef priority_queue<int, vector<int>, gt> minq;
+typedef long long ll;
+typedef pair<ll,ll> pll;
+const ll LINF = 1e18L + 1;
+const int INF = 1e9 + 1;
+//clang++ -std=c++11 -stdlib=libc++ 
+
+int main() {
+  int n; cin >> n;
+  vi a(n);
+  rep(i, n) cin >> a[i];
+  sort(a.begin(), a.end());
+  int h = a.back();
+  int d=0;
+  int idx = -1;
+  rep(i,n-1) {
+    if (d<min(h-a[i], a[i])) {
+      idx = i;
+      d = min(h-a[i], a[i]);
+    }
+  } 
+  printf("%d %d\n", h, a[idx]);
+  return 0;
+}
