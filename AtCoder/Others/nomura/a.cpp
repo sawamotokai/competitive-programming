b049e0547db56f2f57f3a6b2e2e38d594ecc9078
Kai Sawamoto <kaisawamoto@gmail.com>
2020-05-30 22:11:29 +0900

    union find

---
+++ AtCoder/Others/nomura/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ok() puts(ok?"Yes":"No");
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef greater<int> gt;
+typedef priority_queue<int, vector<int>, gt> minq;
+typedef long long ll;
+typedef pair<ll,ll> pll;
+const ll LINF = 1e18L + 1;
+const int INF = 1e9 + 1;
+//clang++ -std=c++11 -stdlib=libc++ 
+
+int main() {
+  int h1, m1, h2, m2, k; cin>>h1>>m1>>h2>>m2>>k;
+  int mins = h2*60 + m2 - h1*60 - m1;
+  int ans = max(0, mins-k);
+  cout << ans << endl;
+  return 0;
+}
