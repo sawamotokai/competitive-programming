5d4d0feaad5fda8fde731d7b9d4e51f943bb3e90
Kai Sawamoto <kaisawamoto@gmail.com>
2020-07-08 23:51:16 +0900

    7/8

---
+++ AtCoder/shojin/skyblue/29.cpp
@@ -0,0 +1,26 @@
+// abc113d
+#include <bits/stdc++.h>
+#include <cassert>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ok() puts(ok?"Yes":"No");
+#define chmax(x,y) x = max(x,y)
+#define chmin(x,y) x = min(x,y)
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using ii = pair<int, int>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using gt = greater<int>;
+using minq = priority_queue<int, vector<int>, gt>;
+using P = pair<ll,ll>;
+const ll LINF = 1e18L + 1;
+const int INF = 1e9 + 1;
+//clang++ -std=c++11 -stdlib=libc++ 
+const int mod = 1e9 + 7;
+int main() {
+  int h,w,k; cin >> h >> w >> k;
+   
+  return 0;
+}
