fdc5296c4a853724e6a6b19a5b5753b94a7218ab
Kai Sawamoto <kaisawamoto@gmail.com>
2020-07-09 00:59:55 +0900

    7/8

--- AtCoder/shojin/skyblue/29.cpp
+++ AtCoder/shojin/skyblue/29.cpp
@@ -19,8 +19,26 @@ const ll LINF = 1e18L + 1;
 const int INF = 1e9 + 1;
 //clang++ -std=c++11 -stdlib=libc++ 
 const int mod = 1e9 + 7;
+
+ll dp[101][10];
 int main() {
   int h,w,k; cin >> h >> w >> k;
-   
+  dp[0][0] = 1;
+  rep(i, h) {
+    rep(j, w) {
+      rep(mask, 1<<(w-1)) {
+        bool ok = true;
+        rep(bit, w-2) {
+          if ((mask >> bit)& 1 && (mask >> (bit+1)) & 1) ok = false;
+        }
+        if (!ok) continue;
+        int nj = j;
+        if ((mask >> j) & 1) nj++;
+        else if ((mask >> (j - 1)) & 1 && j > 0) nj--;
+        (dp[i+1][nj] += dp[i][j]) %= mod;
+      }
+    }
+  }
+  printf("%lld\n", dp[h][k-1]);
   return 0;
 }
