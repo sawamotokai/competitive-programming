f4087d55859060b7095d70f4ed598a71b976940b
Kai Sawamoto <kaisawamoto@gmail.com>
2020-08-01 23:31:24 +0900

    8/1

--- AtCoder/shojin/skyblue/67.cpp
+++ AtCoder/shojin/skyblue/67.cpp
@@ -17,28 +17,29 @@ using P = pair<ll,ll>;
 const ll LINF = 1e18L + 1;
 const int INF = 1e9 + 1;
 //clang++ -std=c++11 -stdlib=libc++ 
-
+ 
 int A,B,C; 
-long double f(long double t) {
-  long double ret = B*sin(C*t*M_PI) + A*t;
+double f(double t) {
+  double ret = B*sin(C*t*M_PI) + A*t;
   return ret;
 }
-
+ 
 int main() {
   cin >> A >> B >> C;
-  long double lo = 0.;
-  long double hi = 1e9 + 1.0;
-  long double ans = 0;
-  long double mid = 0;
+  double lo = 0.;
+  double hi = 1e9 + 1.0;
+  double ans = 0;
+  double mid = 0;
   while (abs(ans-100.) > 1e-6) {
     mid = (lo+hi)/2.;
     ans = f(mid);
-    if (abs(ans-100.) <= 1e-6) {
-      printf("%.10Lf\n", mid);
+    if (ans == 100) {
+      cout << mid << endl;
       return 0;
     }
     if (ans < 100) lo = mid;
     else hi = mid;
   }
+  printf("%.11f\n", mid);
   return 0;
 }
