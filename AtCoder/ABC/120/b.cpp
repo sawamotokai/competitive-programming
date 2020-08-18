653b6cba5704042c15b855a97ffc6d47ed5e3375
Kai Sawamoto <kaisawamoto@gmail.com>
2020-08-18 22:16:40 +0900

    8/18

---
+++ AtCoder/ABC/120/b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <cassert>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ok() puts(ok?"Yes":"No");
+template<class T> bool chmax(T &a, const T &b) { if(a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if(a > b) { a = b; return true; } return false; }
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using ii = pair<int, int>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using gt = greater<int>;
+using minq = priority_queue<int, vector<int>, gt>;
+using P = pair<ll,ll>;
+const ll LINF = 1e18L + 1;
+const int INF = 1e9 + 1;
+//clang++ -std=c++11 -stdlib=libc++ 
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+int A,B,K;
+int main() {
+  cin >> A >> B >> K;
+  ll n = gcd(A,B);
+  int cnt = 0;
+  for (int i=n; i > 0; i--) {
+    if (n % i != 0) continue;
+    cnt++;
+    if (cnt == K) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
+
