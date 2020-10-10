1e6bdb639c0ad5bff74b765f5aa6154aa4cac946
Kai Sawamoto <kaisawamoto@gmail.com>
2020-10-10 22:46:52 +0900

    10/9

---
+++ AtCoder/ABC/hhkb2020/b.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <cassert>
+#define rep(i, N) for (int i = 0; i < (N); ++i)
+#define rep2(i, a, b) for (ll i = a; i <= b; ++i)
+#define rep3(i, a, b) for (ll i = a; i >= b; --i)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define all(c) begin(c), end(c)
+#define ok() puts(ok ? "Yes" : "No");
+template <class T> bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T> bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using ii = pair<int, int>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using gt = greater<int>;
+using minq = priority_queue<int, vector<int>, gt>;
+using P = pair<ll, ll>;
+const ll LINF = 1e18L + 1;
+const int INF = 1e9 + 1;
+// clang++ -std=c++11 -stdlib=libc++
+
+int H, W;
+int main() {
+  cin >> H >> W;
+  vector<string> grid(H);
+  rep(i, H) cin >> grid[i];
+  int ans = 0;
+  rep(i, H) {
+    rep(j, W) {
+      if (j != W - 1 && grid[i][j] == '.' && grid[i][j + 1] == '.')
+        ans++;
+      if (i != H - 1 && grid[i][j] == '.' && grid[i + 1][j] == '.')
+        ans++;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
