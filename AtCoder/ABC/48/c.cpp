017d87768c6e140842b36c564e12ec8289f3b1ef
Kai Sawamoto <kaisawamoto@gmail.com>
2020-12-11 15:38:01 +0900

    12/1

---
+++ AtCoder/ABC/48/c.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <cassert>
+#define rep(i, N) for (int i = 0; i < (N); ++i)
+#define rep2(i, a, b) for (ll i = a; i <= b; ++i)
+#define rep3(i, a, b) for (ll i = a; i >= b; --i)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define all(c) begin(c), end(c)
+#define ok() puts(ok ? "Yes" : "No");
+template <class T> bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T> bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using ii = pair<int, int>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using gt = greater<int>;
+using minq = priority_queue<int, vector<int>, gt>;
+using P = pair<ll, ll>;
+template <class T> void takeUnique(vector<T> &v) {
+  auto last = std::unique(v.begin(), v.end());
+  v.erase(last, v.end());
+}
+const ll LINF = 1e18L + 1;
+const int INF = 1e9 + 1;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+int dxx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dyy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+// clang++ -std=c++11 -stdlib=libc++
+
+int N;
+ll X;
+ll a[100005];
+int main() {
+  cin >> N >> X;
+  rep(i, N) cin >> a[i];
+  ll ans = 0;
+  rep(i, N) {
+    if (X < a[i]) {
+      ans += a[i] - X;
+      a[i] = X;
+    }
+  }
+  rep2(i, 1, N - 1) {
+    ll sum = a[i] + a[i - 1];
+    ll dif = sum - X;
+    if (dif > 0) {
+      a[i] -= dif;
+      ans += dif;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
