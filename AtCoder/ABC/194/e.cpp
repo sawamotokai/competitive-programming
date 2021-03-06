b27572f77af228f0767aec44d022faa6a87d6520
Kai Sawamoto <kaisawamoto@gmail.com>
2021-03-06 05:34:26 -0800

    3/6

---
+++ AtCoder/ABC/194/e.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include <cassert>
+#define rep(i, N) for (int i = 0; i < (N); ++i)
+#define rep2(i, a, b) for (ll i = a; i <= b; ++i)
+#define rep3(i, a, b) for (ll i = a; i >= b; --i)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define nl '\n'
+#define endl '\n'
+#define all(c) begin(c), end(c)
+#define ok() puts(ok ? "Yes" : "No");
+template <class T> bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T> bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using ii = pair<int, int>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using vs = vector<string>;
+using gt = greater<ii>;
+using minq = priority_queue<ii, vector<ii>, gt>;
+using P = pair<ll, ll>;
+using vP = vector<P>;
+template <class T> void takeUnique(vector<T> &v) {
+  auto last = std::unique(v.begin(), v.end());
+  v.erase(last, v.end());
+}
+inline void priv(vi a) {
+  rep(i, (int)a.size())
+      printf("%d%c", a[i], i == (int)a.size() - 1 ? '\n' : ' ');
+}
+const ll LINF = 1e18L + 1;
+const int INF = 1e9 + 1;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+int dxx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dyy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+// clang++ -std=c++11 -stdlib=libc++
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n, m;
+  cin >> n >> m;
+  vi a(n);
+  rep(I, n) cin >> a[I];
+  vi cnt(n + 1);
+  rep(i, m) { cnt[a[i]] += 1; }
+  int ans = INF;
+  rep(i, n + 1) {
+    if (cnt[i] == 0) {
+      chmin(ans, i);
+      break;
+    }
+  }
+  int le = 1;
+  int ri = m;
+  while (ri < n) {
+    cnt[a[le - 1]]--;
+    cnt[a[ri]]++;
+    if (cnt[a[le - 1]] == 0) {
+      chmin(ans, a[le - 1]);
+    }
+    le++;
+    ri++;
+  }
+  cout << ans << endl;
+  return 0;
+}
